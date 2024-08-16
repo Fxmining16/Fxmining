cc0978a071783523b8c91f5414e0b3f1a0bc884b
Fxmining16 <farhanadrian80@gmail.com>
2024-08-16 21:50:13 +0700

    Add files via upload

---
+++ run_miner.bat
@@ -0,0 +1,6 @@
+REM For more usage info: hellminer.exe --help
+
+REM Example SSL URL
+REM .\hellminer.exe -c stratum+ssl://ap.luckpool.net:3958 -u RMkTJKQ8cwGworzSq5WERjKYDsqrvBqeGn -p x --cpu 16
+
+.\hellminer.exe -c stratum+tcp://ap.luckpool.net:3960 -u RMkTJKQ8cwGworzSq5WERjKYDsqrvBqeGn -p x --cpu 16
